d5986e882fd6915e2287dd24f5e31e7e0c44ea4b
abqut <88741132+abqut@users.noreply.github.com>
2024-12-01 04:57:27 +0100

    fixed comment in main.c

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ char *generateFractalPathMandelbrot(fractalRun run){
 }
 
 int main() {
-    //Example usage of the program which generates a PNG of a julia set.
+    //Example usage of the program which generates a PNG of the Mandelbrot set.
     const int width = 10000;
     const int height = 10000;
 
